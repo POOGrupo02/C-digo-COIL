f768b3af05ac293adb8d15c8663f675591bc7d51
leona <leona@DESKTOP-QPSVVE9>
2025-05-10 21:42:44 -0500

    commit 2 coil

--- Código_COIL/src/Clase_abuelo/Personaje_Base.java
+++ Código_COIL/src/Clase_abuelo/Personaje_Base.java
@@ -1,5 +1,45 @@
 package Clase_abuelo;
 
 public abstract class Personaje_Base {
+protected String nombre;
+protected int nivel,salud,resistencia,velocidad;
+public Personaje_Base(String nombre, int nivel, int salud, int resistencia, int velocidad) {
+	super();
+	this.nombre = nombre;
+	this.nivel = nivel;
+	this.salud = salud;
+	this.resistencia = resistencia;
+	this.velocidad = velocidad;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre = nombre;
+}
+public int getNivel() {
+	return nivel;
+}
+public void setNivel(int nivel) {
+	this.nivel = nivel;
+}
+public int getSalud() {
+	return salud;
+}
+public void setSalud(int salud) {
+	this.salud = salud;
+}
+public int getResistencia() {
+	return resistencia;
+}
+public void setResistencia(int resistencia) {
+	this.resistencia = resistencia;
+}
+public int getVelocidad() {
+	return velocidad;
+}
+public void setVelocidad(int velocidad) {
+	this.velocidad = velocidad;
+}
 
 }
